2ea56139eee9685ba563873edb08a32fc872b20e
reza <reza.faieghi@torontomu.ca>
2024-10-03 21:29:14 -0400

    L4 codes pushed before Oct 4 lecture

--- lesson4_code.py
+++ lesson4_code.py
@@ -9,12 +9,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
 
 
 df = pd.read_csv("data/housing.csv")
-
-print(df.info())
+df = df.dropna()
+df = df.reset_index(drop=True)
+# print(df.info())
 
 """One Hote Encoding"""
 # sklearn uses sparse data structures.
@@ -95,22 +97,26 @@ X_test = scaled_data_test_df.join(X_test.iloc[:,-5:])
 
 
 """Correlation Matrix"""
+# Visualize the correlation matrix 
 corr_matrix = (X_train.iloc[:,0:-5]).corr()
 sns.heatmap(np.abs(corr_matrix))
 
-corr1 = y_train.corr(X_train['longitude'])
-print(corr1)
-corr2 = y_train.corr(X_train['latitude'])
-print(corr2)
-corr3 = y_train.corr(X_train['total_rooms'])
-print(corr3)
-corr4 = y_train.corr(X_train['total_bedrooms'])
-print(corr4)
-corr5 = y_train.corr(X_train['population'])
-print(corr5)
-corr6 = y_train.corr(X_train['households'])
-print(corr6)
+# Based on inspection of the corr_matrix, determine colinear variables
+# and explore their correlation with y.
+# corr1 = y_train.corr(X_train['longitude'])
+# print(corr1)
+# corr2 = y_train.corr(X_train['latitude'])
+# print(corr2)
+# corr3 = y_train.corr(X_train['total_rooms'])
+# print(corr3)
+# corr4 = y_train.corr(X_train['total_bedrooms'])
+# print(corr4)
+# corr5 = y_train.corr(X_train['population'])
+# print(corr5)
+# corr6 = y_train.corr(X_train['households'])
+# print(corr6)
 
+#Drop colinear variables that has low correlation with y
 X_train = X_train.drop(['longitude'], axis=1)
 X_train = X_train.drop(['total_bedrooms'], axis=1)
 X_train = X_train.drop(['population'], axis=1)
@@ -121,70 +127,81 @@ X_test = X_test.drop(['total_bedrooms'], axis=1)
 X_test = X_test.drop(['population'], axis=1)
 X_test = X_test.drop(['households'], axis=1)
 
-
+#Double-check correlation matrix, make sure you have no colinear variables left
 plt.figure()
 corr_matrix_2 = (X_train.iloc[:,0:-5]).corr()
 sns.heatmap(np.abs(corr_matrix_2))
 
+#Linear Regression
+linear_reg = LinearRegression()
+param_grid_lr = {}  # No hyperparameters to tune for plain linear regression, but you still apply GridSearchCV.
+grid_search_lr = GridSearchCV(linear_reg, param_grid_lr, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+grid_search_lr.fit(X_train, y_train)
+best_model_lr = grid_search_lr.best_estimator_
+print("Best Linear Regression Model:", best_model_lr)
 
+#Support Vector Machine (SVM)
+svr = SVR()
+param_grid_svr = {
+    'kernel': ['linear', 'rbf'],
+    'C': [0.1, 1, 10, 100],
+    'gamma': ['scale', 'auto']
+}
+grid_search_svr = GridSearchCV(svr, param_grid_svr, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+grid_search_svr.fit(X_train, y_train)
+best_model_svr = grid_search_svr.best_estimator_
+print("Best SVM Model:", best_model_svr)
 
-# """Training First Model"""
-# my_model1 = LinearRegression()
-# my_model1.fit(X_train, y_train)
-# y_pred_train1 = my_model1.predict(X_train)
+# Decision Tree
+decision_tree = DecisionTreeRegressor(random_state=42)
+param_grid_dt = {
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+grid_search_dt = GridSearchCV(decision_tree, param_grid_dt, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+grid_search_dt.fit(X_train, y_train)
+best_model_dt = grid_search_dt.best_estimator_
+print("Best Decision Tree Model:", best_model_dt)
 
-# for i in range(5):
-#     print("Predictions:", y_pred_train1[i], "Actual values:", y_train[i])
+# Random Forest
+random_forest = RandomForestRegressor(random_state=42)
+param_grid_rf = {
+    'n_estimators': [10, 30, 50],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['sqrt', 'log2']
+}
+grid_search_rf = GridSearchCV(random_forest, param_grid_rf, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
+grid_search_rf.fit(X_train, y_train)
+best_model_rf = grid_search_rf.best_estimator_
+print("Best Random Forest Model:", best_model_rf)
 
-# mae_train1 = mean_absolute_error(y_pred_train1, y_train)
-# print("Model 1 training MAE is: ", round(mae_train1,2))
+# Training and testing error for Linear Regression
+y_train_pred_lr = best_model_lr.predict(X_train)
+y_test_pred_lr = best_model_lr.predict(X_test)
+mae_train_lr = mean_absolute_error(y_train, y_train_pred_lr)
+mae_test_lr = mean_absolute_error(y_test, y_test_pred_lr)
+print(f"Linear Regression - MAE (Train): {mae_train_lr}, MAE (Test): {mae_test_lr}")
 
+# Training and testing error for SVM
+y_train_pred_svr = best_model_svr.predict(X_train)
+y_test_pred_svr = best_model_svr.predict(X_test)
+mae_train_svr = mean_absolute_error(y_train, y_train_pred_svr)
+mae_test_svr = mean_absolute_error(y_test, y_test_pred_svr)
+print(f"SVM - MAE (Train): {mae_train_svr}, MAE (Test): {mae_test_svr}")
 
+# Training and testing error for Decision Tree
+y_train_pred_dt = best_model_dt.predict(X_train)
+y_test_pred_dt = best_model_dt.predict(X_test)
+mae_train_dt = mean_absolute_error(y_train, y_train_pred_dt)
+mae_test_dt = mean_absolute_error(y_test, y_test_pred_dt)
+print(f"Decision Tree - MAE (Train): {mae_train_dt}, MAE (Test): {mae_test_dt}")
 
-# """Cross Validation Model 1"""
-# my_model1 = LinearRegression()
-# cv_scores_model1 = cross_val_score(my_model1, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
-# cv_mae1 = -cv_scores_model1.mean()
-# print("Model 1 Mean Absolute Error (CV):", round(cv_mae1, 2))
-# """NOTE THAT THERE IS DATA LEAK IN THIS WAY OF IMPLEMENTING CV. WHY?"""
-
-
-# """Training Second Model"""
-# my_model2 = RandomForestRegressor(n_estimators=30, random_state=42)
-# my_model2.fit(X_train, y_train)
-# y_pred_train2 = my_model2.predict(X_train)
-# mae_train2 = mean_absolute_error(y_pred_train2, y_train)
-# print("Model 2 training MAE is: ", round(mae_train2,2))
-
-
-
-# """Cross Validation Model 2"""
-# cv_scores_model2 = cross_val_score(my_model2, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
-# cv_mae2 = -cv_scores_model2.mean()
-# print("Model 2 Mean Absolute Error (CV):", round(cv_mae2, 2))
-# """NOTE THAT THERE IS DATA LEAK IN THIS WAY OF IMPLEMENTING CV. WHY?"""
-
-
-# for i in range(5):
-#     print("Mode 1 Predictions:",
-#           round(y_pred_train1[i],2),
-#           "Mode 2 Predictions:",
-#           round(y_pred_train2[i],2),
-#           "Actual values:",
-#           round(y_train[i],2))
-
-
-# """GridSearchCV"""
-# param_grid = {
-#     'n_estimators': [10, 30, 50],
-#     'max_depth': [None, 10, 20, 30],
-#     'min_samples_split': [2, 5, 10],
-#     'min_samples_leaf': [1, 2, 4],
-#     'max_features': ['sqrt', 'log2']
-# }
-# my_model3 = RandomForestRegressor(random_state=42)
-# grid_search = GridSearchCV(my_model3, param_grid, cv=5, scoring='neg_mean_absolute_error', n_jobs=1)
-# grid_search.fit(X_train, y_train)
-# best_params = grid_search.best_params_
-# print("Best Hyperparameters:", best_params)
-# best_model3 = grid_search.best_estimator_
+# Training and testing error for Random Forest
+y_train_pred_rf = best_model_rf.predict(X_train)
+y_test_pred_rf = best_model_rf.predict(X_test)
+mae_train_rf = mean_absolute_error(y_train, y_train_pred_rf)
+mae_test_rf = mean_absolute_error(y_test, y_test_pred_rf)
+print(f"Random Forest - MAE (Train): {mae_train_rf}, MAE (Test): {mae_test_rf}")
